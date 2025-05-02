6556b5f676a9c7a2369af8a6d36f058832d46090
denis-home <denisss.dev@gmail.com>
2025-05-02 15:00:40 -0300

    criacao do projeto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <section className="home">
+      <div className="container">
+        <h2>Bem-vindo à nossa paróquia</h2>
+        <p>
+          Nossa missão é evangelizar, acolher e servir. Participe de nossas celebrações e eventos.
+        </p>
+      </div>
+    </section>
+  );
+}
